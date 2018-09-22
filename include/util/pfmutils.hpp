d7965f3ac456f4c197bd613c4f1f544a6b21b1d6
smukherjee2016 <sabyasachimukherjee2006@gmail.com>
2018-09-22 17:12:58 +0900

    Add PFM utilities header

---
+++ include/util/pfmutils.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+struct PFMInfo {
+	std::vector<glm::vec3> image;
+	int w = 1;
+	int h = 1;
+};
+
+PFMInfo readPFM(std::string file) {
+	std::ifstream inFile(file, std::ios::binary | std::ios::in);
+	if (!inFile) {
+		throw std::runtime_error("Cannot open input file: " + file);
+	}
+	std::string type;
+	int w, h;
+	float sFEndianness;
+	inFile >> type;
+	if (type.compare("PF") != 0) {
+		throw std::runtime_error("Invalid or unsupported PFM format, sorry.");
+	}
+	inFile >> w >> h;
+	inFile >> sFEndianness;
+	char dummy;
+	dummy = inFile.get();
+	if (dummy == '\r')
+		inFile.get();
+	//std::cout << type << "  " << w << "  " << h << "  " << sFEndianness << "\n";
+	PFMInfo returnPFM;
+	returnPFM.w = w;
+	returnPFM.h = h;
+	returnPFM.image.resize(w*h);
+	returnPFM.image.clear();
+	//Ignore scale
+	if (sFEndianness < 0) { //Big endian, the usual processing 
+		while (!inFile.eof()) {
+			float r, g, b;
+			inFile.read((char*)(&r), sizeof(float));
+			inFile.read((char*)(&g), sizeof(float));
+			inFile.read((char*)(&b), sizeof(float));
+			returnPFM.image.emplace_back(glm::vec3(r, g, b));
+			inFile.peek(); //To set EOF if we have reached EOF, to prevent an extra dummy iteration of this loop
+
+		}
+
+	}
+	else //Little endian, process each bit separately
+	{
+		while (!inFile.eof()) {
+			//https://stackoverflow.com/questions/19081385/c-read-big-endian-binary-float
+			unsigned char temp[sizeof(float)];
+			inFile.read(reinterpret_cast<char*>(temp), sizeof(float));
+			unsigned char t = temp[0];
+			temp[0] = temp[3];
+			temp[3] = t;
+			t = temp[1];
+			temp[1] = temp[2];
+			temp[2] = t;
+			float r = reinterpret_cast<float&>(temp);
+
+			inFile.read(reinterpret_cast<char*>(temp), sizeof(float));
+			t = temp[0];
+			temp[0] = temp[3];
+			temp[3] = t;
+			t = temp[1];
+			temp[1] = temp[2];
+			temp[2] = t;
+			float g = reinterpret_cast<float&>(temp);
+
+			inFile.read(reinterpret_cast<char*>(temp), sizeof(float));
+			t = temp[0];
+			temp[0] = temp[3];
+			temp[3] = t;
+			t = temp[1];
+			temp[1] = temp[2];
+			temp[2] = t;
+			float b = reinterpret_cast<float&>(temp);
+
+			returnPFM.image.emplace_back(glm::vec3(r, g, b));
+			inFile.peek(); //To set EOF if we have reached EOF, to prevent an extra dummy iteration of this loop
+
+		}
+		
+	}
+	inFile.close();
+	return returnPFM;
+}
+
+void writePFM(std::string file, std::vector<glm::vec3> image, int w, int h) {
+	//Write Big-endian colorful image
+	std::ofstream outFile(file, std::ios::out | std::ios::binary);
+	float sfEndianness = -1.0f;
+	outFile << "PF" << std::endl << w << " " << h << std::endl << sfEndianness << std::endl;
+	for (auto& v : image) {
+		outFile.write(reinterpret_cast<char*>(&v.x), sizeof(float));
+		outFile.write(reinterpret_cast<char*>(&v.y), sizeof(float));
+		outFile.write(reinterpret_cast<char*>(&v.z), sizeof(float));
+	}
+	outFile.close();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <glm/vec3.hpp>
+#include <util/pfmutils.hpp>
 
 int main(void) {
     std::cout << "Hello RT world" << std::endl;
     glm::vec3 vector = {0, 0,0};
+
 }
