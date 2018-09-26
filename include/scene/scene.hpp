bb97200e07713fec3999e53f9c4cf97dc72225bb
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-09-27 00:53:37 +0900

    Fix compilation error

--- include/scene/scene.hpp
+++ include/scene/scene.hpp
@@ -10,6 +10,6 @@ public:
     std::vector<Sphere> spheres;
 
     void makeScene() {
-        spheres.emplace_back(std::make_unique<Sphere>(Vector3(0.0, 0.0, -1.0), 1.0));
+        spheres.emplace_back(Sphere(Vector3(0.0, 0.0, -1.0), 1.0));
     }
 };
