ee32768473dda47a77fa39819e0113983ff4d512
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-09-27 20:40:53 +0900

    Start implementing planes

---
+++ include/hitables/plane.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "hitable.hpp"
+
+class Plane : public Hitable {
+    bool didItHitSomething(const Ray& ray) {
+
+    }
+    HitInfo returnClosestHit(const Ray& ray) {
+
+    }
+};
