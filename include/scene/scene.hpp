9d55402d3ac0e91e625c2b12421a223bfb2ac773
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-09-27 23:06:25 +0900

    Revise scene implementation to accept different types of Hitables

--- include/scene/scene.hpp
+++ include/scene/scene.hpp
@@ -3,13 +3,15 @@
 #include <memory>
 #include "common/common.hpp"
 #include "hitables/sphere.hpp"
+#include "hitables/plane.hpp"
 
 class Scene
 {
 public:
-    std::vector<Sphere> spheres;
+    std::vector<std::unique_ptr<Hitable>> hitables;
 
     void makeScene() {
-        spheres.emplace_back(Sphere(Vector3(0.0, 0.0, -1.0), 0.45));
+
+        hitables.emplace_back(std::make_unique<Sphere>(Vector3(0.0, 0.0, -1.0), 0.45));
     }
 };
