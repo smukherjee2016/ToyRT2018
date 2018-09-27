390ff5287b26b50957b41b61b87fb8bf1b17adbf
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-09-27 22:46:37 +0900

    Tentatively finish ray-plane intersection and plane making

--- include/hitables/plane.hpp
+++ include/hitables/plane.hpp
@@ -1,12 +1,36 @@
 #pragma once
 
 #include "hitable.hpp"
-
+/*
+ * A plane is defined by a point and a normal.
+ */
 class Plane : public Hitable {
-    bool didItHitSomething(const Ray& ray) {
+    Point3 distanceFromWorldOrigin;
+    Vector3 normal;
 
+    Plane(const Point3& _distanceFromWorldOrigin, const Vector3& _normal) {
+        distanceFromWorldOrigin = _distanceFromWorldOrigin;
+        normal = _normal;
+    }
+
+    bool didItHitSomething(const Ray& ray) {
+        const Float epsilon = 1e-6;
+        Float denominator = glm::dot(ray.d, normal);
+        if(denominator < epsilon )
+            return false;
+
+        Float numerator = glm::dot((distanceFromWorldOrigin - ray.o), normal);
+        Float tSolution = numerator / denominator;
+        if(tSolution <= 0.0)
+            return false;
     }
     HitInfo returnClosestHit(const Ray& ray) {
+        Float denominator = glm::dot(ray.d, normal);
+        Float numerator = glm::dot((distanceFromWorldOrigin - ray.o), normal);
 
+        HitInfo hitInfo;
+        hitInfo.tIntersection = numerator / denominator;
+        hitInfo.normal = normal;
     }
+
 };
