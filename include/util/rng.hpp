38efe56e6a181079d3e6630f43a1dd96d78348e2
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-09-30 18:55:07 +0900

    Add PCG RNG header and test usage

--- include/util/rng.hpp
+++ include/util/rng.hpp
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <random>
+#include "pcg_random.hpp"
+#include "common/common.hpp"
+
+class RNG {
+public:
+    RNG() {
+        pcg_extras::seed_seq_from<std::random_device> seed_source;
+        rng.seed(seed_source);
+    }
+
+    Float generate1DUniform() {
+        std::uniform_real_distribution<Float> uniformRealDistribution(0.0, 1.0);
+        return uniformRealDistribution(rng);
+    }
+private:
+    pcg64_oneseq rng;
+
+};
+
+static thread_local RNG rng;
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,14 @@
 #include "integrators/toyIntegrator.hpp"
 #include "film/film.hpp"
 #include "scene/scene.hpp"
+#include "util/rng.hpp"
 
 int main(void) {
 
     int resX = 1024;
     int resY = 576;
+
+    std::cout << rng.generate1DUniform() << std::endl;
 
     Film film(M_PI/4.0, 1.0, resX, resY);
     PinholeCamera pinholeCamera(Point3(-1.0, 4.0, 5.0), Point3(1.0, -4.0, -5.0), Point3(0.0, 1.0, 0.0));
