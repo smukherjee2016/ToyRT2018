07aaf1d5effe92b170c1a4840bd4db8b4fbad474
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-10-01 02:13:31 +0900

    Avoid putting env map in emitters for now

--- include/scene/scene.hpp
+++ include/scene/scene.hpp
@@ -10,13 +10,13 @@ class Scene
 {
 public:
     std::vector<std::unique_ptr<Hitable>> hitables;
-    std::vector<std::shared_ptr<Emitter>> emitters;
+    std::vector<std::unique_ptr<Emitter>> emitters;
     std::shared_ptr<EnvironmentMap> envMap;
 
     void makeScene() {
 
         envMap = std::make_shared<EnvironmentMap>();
-        emitters.emplace_back(envMap);
+        //emitters.emplace_back(envMap);
 
         hitables.emplace_back(std::make_unique<Sphere>(Vector3(0.0, 0.0, 0.0), 0.7));
         hitables.emplace_back(std::make_unique<Plane>(Point3(0.0, -0.7, 0.0), Vector3(0.0,1.0,0.0)));
