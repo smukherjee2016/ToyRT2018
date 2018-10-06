9c59c1d2d7e304e22420c2414c26eb11e47c915d
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2018-10-06 17:45:23 +0900

    Modify cbox-ish smallpt scene to make normals face inward

--- include/scene/scene.hpp
+++ include/scene/scene.hpp
@@ -24,11 +24,13 @@ public:
 
 
         //objects.emplace_back(std::make_unique<Plane>(Point3(0.0, -0.7, 0.0), Vector3(0.0,1.0,0.0)));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(1e5+1,40.8,81.6)  , 1e5 , std::make_shared<LambertUniform>(Spectrum(.75,.25,.25))));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(-1e5+99,40.8,81.6), 1e5 , std::make_shared<LambertUniform>(Spectrum(.25,.25,.75))));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(50,40.8,1e5)      , 1e5 , std::make_shared<LambertUniform>(Spectrum(.75))));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(50,1e5,81.6)      , 1e5 , std::make_shared<LambertUniform>(Spectrum(.75))));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(50,-1e5+81.6,81.6), 1e5 , std::make_shared<LambertUniform>(Spectrum(.75))));
+
+        //Cbox-ish, source: smallpt, modified to make flipping normals unnecessary
+        objects.emplace_back(std::make_unique<Sphere>(Point3(1e5+99,40.8,81.6)  , 1e5 , std::make_shared<LambertUniform>(Spectrum(.25,.25,.75)))); // Right wall (after modification
+        objects.emplace_back(std::make_unique<Sphere>(Point3(-1e5+1,40.8,81.6), 1e5 , std::make_shared<LambertUniform>(Spectrum(.75,.25,.25)))); //Left wall (after modification)
+        objects.emplace_back(std::make_unique<Sphere>(Point3(50,40.8,-1e5)      , 1e5 , std::make_shared<LambertUniform>(Spectrum(.75)))); //Front wall (after modification)
+        objects.emplace_back(std::make_unique<Sphere>(Point3(50,1e5+81.6,81.6)      , 1e5 , std::make_shared<LambertUniform>(Spectrum(.75)))); //Ceiling (after modification)
+        objects.emplace_back(std::make_unique<Sphere>(Point3(50,-1e5,81.6), 1e5 , std::make_shared<LambertUniform>(Spectrum(.75)))); //Floor (after modification)
         objects.emplace_back(std::make_unique<Sphere>(Point3(27,16.5,47)       , 16.5, std::make_shared<LambertUniform>(Spectrum(.999))));
         objects.emplace_back(std::make_unique<Sphere>(Point3(73,16.5,78)       , 16.5, std::make_shared<LambertUniform>(Spectrum(.999))));
         objects.emplace_back(std::make_unique<Sphere>(Point3(50,681.6-.27,81.6), 600 , std::make_shared<LambertUniform>(Spectrum(0.0)), Spectrum(12)));
