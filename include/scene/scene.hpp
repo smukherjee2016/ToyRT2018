21bd793824c9638564888ddc4fcf997e7cbb14cf
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2018-10-15 20:45:49 +0900

    Modify scene to better bring out strengths of both samplings, MIS+DL works!

--- include/scene/scene.hpp
+++ include/scene/scene.hpp
@@ -34,12 +34,12 @@ public:
         objects.emplace_back(std::make_unique<Sphere>(Point3(-1e5+1,40.8,81.6), 1e5 , std::make_shared<LambertCosine>(Spectrum(.75,.25,.25)))); //Left wall (after modification)
         objects.emplace_back(std::make_unique<Sphere>(Point3(50,40.8,-1e5)      , 1e5 , std::make_shared<LambertCosine>(Spectrum(.75)))); //Front wall (after modification)
         objects.emplace_back(std::make_unique<Sphere>(Point3(50,1e5+81.6,81.6)      , 1e5 , std::make_shared<LambertCosine>(Spectrum(.75)))); //Ceiling (after modification)
-        objects.emplace_back(std::make_unique<Sphere>(Point3(50,-1e5,81.6), 1e5 , std::make_shared<LambertCosine>(Spectrum(.75)))); //Floor (after modification)
+        objects.emplace_back(std::make_unique<Sphere>(Point3(50,-1e5,81.6), 1e5 , std::make_shared<Phong>(Spectrum(0.01, 0.5, 0.999), 100))); //Floor (after modification)
         objects.emplace_back(std::make_unique<Sphere>(Point3(27,16.5,47)       , 16.5, std::make_shared<LambertCosine>(Spectrum(.999))));
         //objects.emplace_back(std::make_unique<Sphere>(Point3(73,16.5,78)       , 16.5, std::make_shared<LambertCosine>(Spectrum(.999))));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(73,16.5,78)       , 16.5, std::make_shared<Phong>(Spectrum(0.01, 0.5, 0.999), 10)));
+        objects.emplace_back(std::make_unique<Sphere>(Point3(73,16.5,78)       , 16.5, std::make_shared<LambertCosine>(Spectrum(0.0001)), Spectrum(10)));
         //objects.emplace_back(std::make_unique<Sphere>(Point3(50,681.6-.27,81.6), 600 , std::make_shared<LambertUniform>(Spectrum(0.0)), Spectrum(12)));
-        objects.emplace_back(std::make_unique<Sphere>(Point3(60,52,81.6), 6 , std::make_shared<LambertCosine>(Spectrum(0.0001)), Spectrum(60)));
+        objects.emplace_back(std::make_unique<Sphere>(Point3(60,52,81.6), 6 , std::make_shared<LambertCosine>(Spectrum(.75))));
         objects.emplace_back(std::make_unique<Sphere>(Point3(50,40.8,1e5-300), 1e5 , std::make_shared<LambertCosine>(Spectrum(.0001)))); //Back wall to block out envmap light to work around Mitsuba bug
 
 
