e783da3ebac9d7138309df496eceba210995a987
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2018-10-22 16:21:20 +0900

    Rewrite and fix PT with BSDF, should work now

--- include/integrators/pt.hpp
+++ include/integrators/pt.hpp
@@ -13,26 +13,27 @@ public:
             int y = positionInFilm / film.screenWidth;
             //int positionInFilm = y * film.screenWidth + x;
 
-            Spectrum pixelValue{};
+            Spectrum pixelValue(0.0);
             for (int j = 0; j < sampleCount; j++) {
 
                 Ray cameraRay = pinholeCamera.generateCameraRay(x, y, film);
                 Ray prevRay = cameraRay;
 
-                Spectrum throughput(1.0);
-                for(int k = 0; k < numBounces; k++) {
+                Spectrum L(0.0);
+                Spectrum Throughput (1.0);
+                for(int k = 1; k <= numBounces; k++) {
                     std::optional<HitBundle> hitBundle = traceRayReturnClosestHit(prevRay, scene);
                     if (hitBundle) {
 
                         HitBundle prevRayHitBundle = hitBundle.value();
 
-                        //If hit the emitter, return its Le
+                        //If hit the emitter, return its Le multiplied by throughput
                         if(prevRayHitBundle.closestObject->isEmitter()){
-                            pixelValue += prevRayHitBundle.closestObject->Le(prevRay);
+                            L = prevRayHitBundle.closestObject->Le(prevRay) * Throughput;
                             break;
                         }
                         else {
-                            //BSDF sampling
+                            //
                             Vector3 outgoingDirection = prevRayHitBundle.closestObject->mat->sampleDirection(-prevRay.d,
                                                                                                                prevRayHitBundle.hitInfo.normal);
                             Spectrum brdf = prevRayHitBundle.closestObject->mat->brdf(outgoingDirection, -prevRay.d,
@@ -44,48 +45,23 @@ public:
                                 break;
 
                             Ray nextRay(prevRayHitBundle.hitInfo.intersectionPoint, outgoingDirection);
-                            std::optional<HitBundle> nextRayHitBundle = traceRayReturnClosestHit(nextRay, scene);
-                            if (nextRayHitBundle) {
-                                HitBundle nextBundle = nextRayHitBundle.value();
-
-                                //If hit a light source, return its Le
-                                if(nextBundle.closestObject->isEmitter()) {
-                                    Float pdfEmitter_BSDFSampling = nextBundle.closestObject->pdfEmitterA(
-                                            nextBundle.hitInfo.intersectionPoint) * scene.pdfSelectEmitter(nextBundle.closestObject);
-
-                                    //Convert the SA BSDF pdf into Area domain for MIS calculation
-                                    Float squaredDistance = glm::length(nextBundle.hitInfo.intersectionPoint - prevRayHitBundle.hitInfo.intersectionPoint) *
-                                                            glm::length(nextBundle.hitInfo.intersectionPoint - prevRayHitBundle.hitInfo.intersectionPoint);
-                                    //Vector3 emitterNormal = nextBundle.closestObject->getNormalForEmitter(nextBundle.hitInfo.intersectionPoint);
-                                    Float pdfBSDFA_BSDFSampling = pdfBSDF_BSDFSampling * glm::dot(outgoingDirection, prevRayHitBundle.hitInfo.normal) / squaredDistance;
-                                    Float misweight = 1.0;//PowerHeuristic(1, pdfBSDFA_BSDFSampling, 1, pdfEmitter_BSDFSampling);
-
-                                    pixelValue += nextBundle.closestObject->Le(nextRay) * throughput * brdf * glm::dot(outgoingDirection, prevRayHitBundle.hitInfo.normal) * misweight / pdfBSDF_BSDFSampling;
-                                }
-                                else {
-                                    throughput *= (brdf * glm::dot(outgoingDirection, prevRayHitBundle.hitInfo.normal) / pdfBSDF_BSDFSampling);
-                                    if(glm::any(glm::equal(throughput, Vector3(0.0f))) || glm::any(glm::isnan(throughput))) break;
-                                    prevRay = nextRay;
-                                }
-
-
-                        }
-                        else {
-                            //Did not hit any emitter so hit env map. Thus get contribution from envmap with losses at material hits
-                            //TODO Stop using env map as a special emitter and merge into existing emitter implementation
-                            if(glm::any(glm::equal(throughput, Vector3(0.0f))) || glm::any(glm::isnan(throughput))) break;
-                            pixelValue += scene.envMap->Le(prevRay) * throughput * brdf * glm::dot(outgoingDirection, prevRayHitBundle.hitInfo.normal) / pdfBSDF_BSDFSampling;
-                            break;
-                        }
+                            Throughput *= (brdf * glm::dot(outgoingDirection, prevRayHitBundle.hitInfo.normal) / pdfBSDF_BSDFSampling);
+                            prevRay = nextRay;
 
                         }
 
                     }
-                    else { //Did not hit any object so hit environment map
-                        pixelValue += scene.envMap->Le(prevRay);
+                    else {
+                        //Did not hit any emitter so hit env map. Thus get contribution from envmap with losses at material hits
+                        //TODO Stop using env map as a special emitter and merge into existing emitter implementation
+                        if(glm::any(glm::equal(Throughput, Vector3(0.0f))) || glm::any(glm::isnan(Throughput))) break;
+                        L = scene.envMap->Le(prevRay) * Throughput;
                         break;
                     }
+
+
                 }
+                pixelValue += L;
 
             }
             pixelValue /= (sampleCount);
