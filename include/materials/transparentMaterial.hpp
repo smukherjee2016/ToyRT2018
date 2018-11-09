3f29e25466d01793a7247a0d06526dc7793f83a8
smukherjee2016 <sabyasachimukherjee2006@gmail.com>
2018-11-09 16:24:40 +0900

    Modify transparent material to be transparent

--- include/materials/transparentMaterial.hpp
+++ include/materials/transparentMaterial.hpp
@@ -5,7 +5,7 @@
 class TransparentMaterial : public Material {
 
     Vector3 sampleDirection(const Vector3& wo, const Vector3& normal) const override {
-        return Vector3(0.0); //Return a direction to nowhere
+        return wo; //Return the same direction. Light passes through unmodified
     }
     Spectrum brdf(const Vector3& wi, const Vector3& wo, const Vector3& normal) const override {
         return Spectrum(1.0);
