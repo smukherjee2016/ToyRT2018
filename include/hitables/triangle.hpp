0393a7e7842fb0359a6623c4114eae63d06dfab1
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2019-01-03 18:40:35 +0900

    Finish fixing compiler errors

--- include/hitables/triangle.hpp
+++ include/hitables/triangle.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
 #include "common/common.hpp"
+#include "objects/object.hpp"
 
 
-class TriangleMesh {
+class TriangleMesh : public Object {
+public:
+    std::optional<HitInfo> checkIntersectionAndClosestHit(const Ray &ray) const override {
+        return std::optional<HitInfo>();
+    }
 
+    Point3 samplePointOnObject(Sampler sampler) const override {
+        return Point3();
+    }
+
+    Float pdfSelectPointOnObjectA(const Point3 &point) const override {
+        return 0;
+    }
+
+    Vector3 getNormalAtPoint(const Point3 &point) const override {
+        return Vector3();
+    }
+
+    Float surfaceArea() const override {
+        return 0;
+    }
+
+    bool isEmitter() const override {
+        return !(emitter == nullptr);
+    }
 };
 
 class Triangle {
